86f7a513595a94d205b9c925d6bf1a9124d4d572
Rpinto30 <rodrigopinto3007@gmail.com>
2025-08-12 21:40:30 -0600

    Creación de un método que me permite automatizar el ingreso de atributos de la clase padre (nombre, precio y peso), para el uso de las demás galletas

--- actividad_19.py
+++ actividad_19.py
@@ -1,3 +1,13 @@
+def input_number(message): #INGRESAR UN NUMERO Y VERIFICAR QUE SU ENTRADA SEA VALIDA
+    while True:
+        try:
+            value = float(input(message))
+            break
+        except ValueError: print('-'*50+'\n'+"❌"*5+"   Lo siento valor no valido, intenta nuevamente   "+"❌"*5)
+
+    if value == int(value): return int(value)
+    else: return value
+
 class Filling:
     def __init__(self, filling_type): self.filling_type = filling_type
     def describe(self): print(f"El relleno de tu galleta es: {self.filling_type}")
@@ -25,8 +35,28 @@ class FillingCookie(Cookie, Filling):
         self.describe()
 
 class CookiesSystem:
+    def __init__(self): self.cookies = []
+
+    def consult_cookie(self):
+        while True:
+            _name = input("Ingrese el nombre de su galleta: ")
+            if _name != '': break
+            print("\nLo sentimos, debe agregar un nombre a la galleta, intente de nuevo")
+
+        while True:
+            _price = input_number("Ingresa el precio de tu galleta: ")
+            if _price >= 0: break
+            print("\nEl precio debe ser mayor o igual a 0, intenta de nuevo por favor")
+
+        while True:
+            _weight = input_number("Ingresa el peso de tu galleta: ")
+            if _weight >= 0: break
+            print("\nEl peso debe ser mayor o igual a 0, intenta de nuevo por favor")
+        return _name, _price, _weight
+
     def add_cookie(self):
-        pass
+        name, price, weight = self.consult_cookie()
+        self.cookies.append(Cookie(name, price, weight))
 
     def add_cookie_chips(self):
         pass
@@ -44,4 +74,7 @@ class CookiesSystem:
         pass
 
 
-co = Cookie('', -1,-2)
+sys_cookie = CookiesSystem()
+
+sys_cookie.add_cookie()
+print(sys_cookie.cookies)
