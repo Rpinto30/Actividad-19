5dd09f267e04a8a3a04d59772d0b24c564875f04
Rpinto30 <rodrigopinto3007@gmail.com>
2025-08-12 21:15:53 -0600

    Elimine las excepciones en el contructor porque no siempre se ejecutaba bien. La clase de galleta rellena está creada en el sistema

--- actividad_19.py
+++ actividad_19.py
@@ -4,30 +4,24 @@ class Filling:
 
 class Cookie:
     def __init__(self, name, price, weight):
-        try:
-            #Encontré una palabra clave "raise" que me permite lanzar una excepción por mi cuenta
-            #https://ellibrodepython.com/excepciones-try-except-finally
-            if name == '': raise NameError("El nombre de la galleta no puede estar vacío")
-            if price <= 0: raise ValueError("El precio de la galleta debe ser mayor que 0")
-            if weight <= 0: raise ValueError("El peso de la galleta debe ser mayor a 0")
-            self.nombre = name
-            self.precio = price
-            self.weight = weight
-        except Exception as e:
-            print(f"Error al crear Producto: {e}")
+        self.name = name
+        self.price = price
+        self.weight = weight
 
 class ChocolateChips(Cookie):
     def __init__(self, name, price, weight, chips):
         # Uso de super() para heredar los atributos del objeto Padre
         #https://ellibrodepython.com/herencia-en-python
         super().__init__(name, price, weight)
-        try:
-            if chips <= 0: raise ValueError("La galleta debe de tener al menos 1 chispa de chocolate")
-            self.chips = chips
-        except Exception as e:
-            print(f"Error al crear Producto: {e}")
+        self.chips = chips
 
-class FillingCookie:
-    pass
+class FillingCookie(Cookie, Filling):
+    def __init__(self, name, price, weight, filling_type):
+        Cookie.__init__(self,name,price,weight)
+        Filling.__init__(self, filling_type)
 
-co = Cookie('name', 0,-2)
+    def show_inf(self):
+        print(f"La galleta: {self.name} Precio: {self.price} | Pesa: {self.weight}")
+        self.describe()
+
+co = Cookie('', -1,-2)
